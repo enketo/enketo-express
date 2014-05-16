b7d63db8378d40f50496e7b32f2081064a1030a4
Martijn van de Rijdt <martijn@enketo.org>
2014-05-16 17:32:31 -0600

    added Vagrantfile

---
+++ setup/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+apt-get update
+
+# install XML prerequisites for node_xslt
+apt-get install -y libxml2-dev libxslt1-dev
+
+# install development tools, node, grunt #, ruby, sass gem
+apt-get install -y python-software-properties python g++ make
+add-apt-repository ppa:chris-lea/node.js
+apt-get update
+apt-get install -y nodejs
+npm install -g grunt-cli nodemon mocha
+cd /vagrant
+# remove node_modules if exists because npm builds can be system-specific
+if [ -d "/vagrant/node_modules" ]; then
+	rm -R /vagrant/node_modules
+fi
+npm install 
+
+# build js and css
+grunt
+
+grunt develop
