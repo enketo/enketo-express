94434eb7bd4d2f422f2f244b9ff6a5652761fb7b
Martijn van de Rijdt <martijn@enketo.org>
2014-06-26 15:38:14 -0600

    switched transformer tests to chai-as-promise, closes #16

--- test/transformer.spec.js
+++ test/transformer.spec.js
@@ -1,31 +1,27 @@
 /* global describe, require, it*/
 "use strict";
 
-var should = require( 'should' );
-var fs = require( "fs" );
-var transformer = require( "../lib/transformer" );
+var Q = require( 'q' ),
+    chai = require( "chai" ),
+    expect = chai.expect,
+    chaiAsPromised = require( "chai-as-promised" ),
+    fs = require( "fs" ),
+    transformer = require( "../lib/transformer" );
 
 describe( 'transformer', function() {
 
     describe( 'transforms valid XForms', function() {
         var xform = fs.readFileSync( './test/forms/widgets.xml' );
-        it( 'without an error', function( done ) {
+        it( 'without an error', function() {
 
-            transformer.transform( {
+            var result = transformer.transform( {
                 xform: xform
-            } )
-                .then( function( result ) {
-                    result.should.be.of.type( 'object' );
-                    result.should.have.property( 'form' );
-                    result.should.have.property( 'model' );
-                    result.form.should.not.be.empty;
-                    result.model.should.not.be.empty;
-                    done();
-                } )
-                .catch( function( error ) {
-                    error.should.not.be.ok;
-                    done();
-                } )
+            } );
+            return Q.all( [
+                expect( result ).to.eventually.to.be.an( 'object' ),
+                expect( result ).to.eventually.have.property( 'form' ).and.to.not.be.empty,
+                expect( result ).to.eventually.have.property( 'model' ).and.to.not.be.empty
+            ] );
         } );
     } );
 
@@ -33,26 +29,12 @@ describe( 'transformer', function() {
         var invalid_xforms = [ undefined, null, '', '<data>' ];
 
         invalid_xforms.forEach( function( xform ) {
-            it( 'with a parse error', function( done ) {
-                var errorMsg,
-                    result = false;
-
-                transformer.transform( {
+            it( 'with a parse error', function() {
+                var result = transformer.transform( {
                     xform: xform
-                } )
-                    .then( function( result ) {
-                        result = result;
-                    } )
-                    .catch( function( error ) {
-                        errorMsg = error.message;
-                    } )
-                    .then( function() {
-                        result.should.not.be.ok;
-                        errorMsg.should.containEql( 'parse' );
-                        done();
-                    } );
+                } );
+                return expect( result ).to.eventually.be.rejectedWith( Error, /parse/ );
             } );
         } );
-
     } );
 } );
