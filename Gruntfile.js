09f83837217c1e13fc73153603bd88cfc908ee7b
Martijn van de Rijdt <martijn@enketo.org>
2014-08-04 13:25:49 -0600

    add compile to default grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -138,7 +138,7 @@ module.exports = function( grunt ) {
         };
     }
 
-    grunt.registerTask( 'default', [ 'symlink', 'test' ] );
+    grunt.registerTask( 'default', [ 'symlink', 'test', 'compile' ] );
     grunt.registerTask( 'compile', [ 'requirejs', 'sass' ] );
     grunt.registerTask( 'test', [ 'symlink', 'mochaTest', 'jsbeautifier:test', 'jshint', 'sass', 'requirejs' ] );
     grunt.registerTask( 'develop', [ 'concurrent:develop' ] );
