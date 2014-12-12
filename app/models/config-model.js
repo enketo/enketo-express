9a2fce841a4be54a3829cf13d7ce4df9843b09a5
Martijn van de Rijdt <martijn@enketo.org>
2014-12-12 16:26:46 -0700

    feature: geotrace/shape widgets allow pasting KML coordinates, Google Maps layers now supported as well

--- app/models/config-model.js
+++ app/models/config-model.js
@@ -5,7 +5,7 @@ var config = require( '../../config/config' );
 module.exports = {
     client: function() {
         return {
-            google_api_key: config.google[ 'api key' ],
+            googleApiKey: config.google[ 'api key' ],
             maps: config.maps,
             widgets: config.widgets,
             modernBrowserURL: 'modern-browsers',
