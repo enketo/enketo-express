b5eaf4f53c3ebc23ae9f66f4e39c653e21b1e71c
Martijn van de Rijdt <martijn@enketo.org>
2015-03-23 12:09:13 -0600

    added: show supported languages on home page

--- app/controllers/pages-controller.js
+++ app/controllers/pages-controller.js
@@ -11,7 +11,8 @@ module.exports = function( app ) {
 router
     .get( '/', function( req, res ) {
         res.render( 'index', {
-            openrosa: req.app.get( 'linked form and data server' ).name || '?'
+            openrosa: req.app.get( 'linked form and data server' ).name || '?',
+            languages: req.app.get( 'languages supported' )
         } );
     } )
     .get( '/modern-browsers', function( req, res ) {
