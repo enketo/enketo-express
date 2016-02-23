enketo/enketo-express

commit a0a9b489323be6924d76bafb4a602339bee8f7b6

changed: upgraded i18next, closes https://github.com/kobotoolbox/enketo-express/issues/266
fixed: error pages return 500 instead of 400
changed: now avoiding synchronous translation XHR requests

config/express.js
@@ -8,7 +8,9 @@ var fs = require( 'fs' );
 var favicon = require( 'serve-favicon' );
 var config = require( '../app/models/config-model' ).server;
 var logger = require( 'morgan' );
-var i18n = require( 'i18next' );
+var i18next = require( 'i18next' );
+var I18nextBackend = require( 'i18next-node-fs-backend' );
+var i18nextMiddleware = require( 'i18next-express-middleware' );
 var compression = require( 'compression' );
 var errorHandler = require( '../app/controllers/error-handler' );
 var controllersPath = path.join( __dirname, '../app/controllers' );
@@ -33,20 +35,26 @@ app.set( 'view engine', 'jade' );
 app.set( 'json spaces', 4 );
 
 // setup i18next
-i18n.init( {
-    // don't bother with these routes
-    ignoreRoutes: [ 'css/', 'fonts/', 'images/', 'js/', 'lib/' ],
-    // only attemp to translate the supported languages
-    supportedLngs: app.get( 'languages supported' ),
-    // allow query string lang override
-    detectLngQS: 'lang',
-    // fallback language
-    fallbackLng: 'en',
-    // don't use cookies, always detect 
-    useCookie: false
-} );
-// make i18n apphelper available in jade templates
-i18n.registerAppHelper( app );
+i18next
+    .use( i18nextMiddleware.LanguageDetector )
+    .use( I18nextBackend )
+    .init( {
+        //debug: true, // DEBUG
+        whitelist: app.get( 'languages supported' ),
+        fallbackLng: 'en',
+        backend: {
+            loadPath: path.resolve( __dirname, '../locales/__lng__/translation.json' )
+        },
+        detection: {
+            order: [ 'querystring', 'header' ],
+            lookupQuerystring: 'lang',
+            caches: false,
+        },
+        interpolation: {
+            prefix: '__',
+            suffix: '__'
+        }
+    } );
 
 // middleware
 app.use( compression() );
@@ -55,7 +63,9 @@ app.use( bodyParser.urlencoded( {
     extended: true
 } ) );
 app.use( cookieParser( app.get( 'encryption key' ) ) );
-app.use( i18n.handle );
+app.use( i18nextMiddleware.handle( i18next, {
+    /*ignoreRoutes: [ '/css', '/fonts', '/images', '/js' ]*/
+} ) );
 app.use( favicon( path.resolve( __dirname, '../public/images/favicon.ico' ) ) );
 app.use( express.static( path.resolve( __dirname, '../public' ) ) );
 app.use( '/locales', express.static( path.resolve( __dirname, '../locales' ) ) );
@@ -69,23 +79,8 @@ app.use( function( req, res, next ) {
     res.locals.logo = req.app.get( 'logo' );
     res.locals.defaultTheme = req.app.get( 'default theme' ).replace( 'theme-', '' ) || 'kobo';
     res.locals.title = req.app.get( 'app name' );
-    res.locals.directionality = function() {
-        // TODO: remove this when https://github.com/i18next/i18next/pull/413 is merged, copied to node-i18next, and published.
-        // After that we can just access i18n.dir(), in the jade template
-        var currentLng = i18n.lng();
-        var rtlLangs = [ 'ar', 'shu', 'sqr', 'ssh', 'xaa', 'yhd', 'yud', 'aao', 'abh', 'abv', 'acm',
-            'acq', 'acw', 'acx', 'acy', 'adf', 'ads', 'aeb', 'aec', 'afb', 'ajp', 'apc', 'apd', 'arb',
-            'arq', 'ars', 'ary', 'arz', 'auz', 'avl', 'ayh', 'ayl', 'ayn', 'ayp', 'bbz', 'pga', 'he',
-            'iw', 'ps', 'pbt', 'pbu', 'pst', 'prp', 'prd', 'ur', 'ydd', 'yds', 'yih', 'ji', 'yi', 'hbo',
-            'men', 'xmn', 'fa', 'jpr', 'peo', 'pes', 'prs', 'dv', 'sam'
-        ];
-
-        if ( rtlLangs.some( function( lang ) {
-                return new RegExp( '^' + lang ).test( currentLng );
-            } ) ) {
-            return 'rtl';
-        }
-        return 'ltr';
+    res.locals.dir = function( lng ) {
+        return i18next.dir( lng );
     };
     next();
 } );
@@ -105,7 +100,8 @@ app.use( logger( ( app.get( 'env' ) === 'development' ? 'dev' : 'tiny' ) ) );
 app.use( errorHandler[ '404' ] );
 if ( app.get( 'env' ) === 'development' ) {
     app.use( errorHandler.development );
+} else {
+    app.use( errorHandler.production );
 }
-app.use( errorHandler.production );
 
 module.exports = app;

public/js/src/module/gui.js
@@ -7,7 +7,7 @@
 var support = require( 'enketo-core/src/js/support' );
 var settings = require( './settings' );
 var printForm = require( 'enketo-core/src/js/print' );
-var t = require( './translator' );
+var t = require( './translator' ).t;
 var sniffer = require( './sniffer' );
 var dialog = require( './vex.dialog.custom' );
 var $ = require( 'jquery' );

public/js/src/module/translator.js
@@ -1,52 +1,70 @@
 'use strict';
 
 var settings = require( './settings' );
-var i18next = require( 'i18next-client' );
+var i18next = require( 'i18next' );
+var XHR = require( 'i18next-xhr-backend' );
+var LanguageDetector = require( 'i18next-browser-languagedetector' );
 var $ = require( 'jquery' );
-
-var options;
+var init;
+var t;
+var htmlParagraphsPostProcessor;
 
 // The postProcessor assumes that array values with line breaks should be divided into HTML paragraphs.
-i18next.addPostProcessor( 'htmlParagraphs', function( value, key ) {
-    var paragraphs = value.split( '\n' );
-    return ( paragraphs.length > 1 ) ? '<p>' + paragraphs.join( '</p><p>' ) + '</p>' : value;
-} );
-
-options = {
-    // path where language files are available
-    // resGetPath: '/locales/__lng__/translation.json',
-    // load a fallback language
-    fallbackLng: 'en',
-    // allow language override with 'lang' query parameter
-    detectLngQS: 'lang',
-    // only load unspecific languages (i.e. without country code - may need to be changed at some stage)
-    load: 'unspecific',
-    // avoid uselessly attempting to obtain unsupported languages
-    lngWhitelist: settings.languagesSupported,
-    // always use htmlLineParagrahs post processor
-    postProcess: 'htmlParagraphs',
-    // don't use cookies, always detect
-    useCookie: false,
-    // use custom loader to avoid query string timestamp (messes up applicationCache)
-    customLoad: function( lng, ns, options, loadComplete ) {
-        // load the file for given language and namespace
-        $.ajax( {
-                url: '/locales/__lng__/translation.json'.replace( '__lng__', lng ),
-                async: false
-            } )
-            .done( function( data ) {
-                loadComplete( null, data );
-            } )
-            .fail( function( error ) {
-                loadComplete( error );
-            } );
+htmlParagraphsPostProcessor = {
+    type: 'postProcessor',
+    name: 'htmlParagraphsPostProcessor',
+    process: function( value, key ) {
+        var paragraphs = value.split( '\n' );
+        return ( paragraphs.length > 1 ) ? '<p>' + paragraphs.join( '</p><p>' ) + '</p>' : value;
     }
 };
 
-i18next.init( options );
+/**
+ * Initializes translator and resolves **when translations have been loaded**.
+ * 
+ * @param  {=*?} something can be anything
+ * @return {Promise}       promise resolving the original something argument
+ */
+init = function( something ) {
+    return new Promise( function( resolve, reject ) {
+        i18next
+            .use( XHR )
+            .use( LanguageDetector )
+            .use( htmlParagraphsPostProcessor )
+            .init( {
+                whitelist: settings.languagesSupported,
+                fallbackLng: 'en',
+                backend: {
+                    loadPath: '/locales/__lng__/translation.json',
+                },
+                detection: {
+                    order: [ 'querystring', 'navigator' ],
+                    lookupQuerystring: 'lang',
+                    caches: []
+                },
+                interpolation: {
+                    prefix: '__',
+                    suffix: '__'
+                },
+                postProcess: [ 'htmlParagraphsPostProcessor' ]
+            }, function( error ) {
+                if ( error ) {
+                    reject( error );
+                } else {
+                    resolve( something );
+                }
+            } );
+    } );
+};
 
-module.exports = i18next.t;
+t = function( key, options ) {
+    return i18next.t( key, options );
+};
 
+module.exports = {
+    init: init,
+    t: t
+};
 
 /**
  * add keys from XSL stylesheets manually

app/controllers/error-handler.js
@@ -47,8 +47,8 @@ module.exports = {
         }
     },
     '404': function( req, res, next ) {
-        var err = new Error( req.i18n.t( 'error.pagenotfound' ) );
-        err.status = 404;
-        next( err );
+        var error = new Error( req.i18n.t( 'error.pagenotfound' ) );
+        error.status = 404;
+        next( error );
     }
 };

app/controllers/manifest-controller.js
@@ -28,7 +28,7 @@ function getManifest( req, res ) {
             if ( err ) {
                 reject( err );
             } else {
-                manifest.get( html, req.i18n.lng() )
+                manifest.get( html, req.i18n.language )
                     .then( resolve )
                     .catch( reject );
             }

app/views/layout.jade
@@ -1,5 +1,5 @@
 doctype html
-html(manifest=manifest, lang=i18n.lng(), dir=directionality())
+html(manifest=manifest, lang=language, dir=dir(language))
   head
     title= title
 

public/js/src/main-webform-edit.js
@@ -9,9 +9,9 @@ var gui = require( './module/gui' );
 var controller = require( './module/controller-webform' );
 var settings = require( './module/settings' );
 var connection = require( './module/connection' );
-var t = require( './module/translator' );
+var translator = require( './module/translator' );
+var t = translator.t;
 var utils = require( './module/utils' );
-
 var $loader = $( '.form__loader' );
 var $form = $( 'form.or' );
 var $buttons = $( '.form-header__button--print, button#submit-form' );
@@ -23,27 +23,31 @@ var survey = {
     instanceId: settings.instanceId
 };
 
-Promise.all( [
-    connection.getFormParts( survey ),
-    connection.getExistingInstance( survey )
-] ).then( function( responses ) {
-    var formParts = responses[ 0 ];
-    formParts.instance = responses[ 1 ].instance;
+translator.init( survey )
+    .then( function( survey ) {
+        return Promise.all( [
+            connection.getFormParts( survey ),
+            connection.getExistingInstance( survey )
+        ] );
+    } )
+    .then( function( responses ) {
+        var formParts = responses[ 0 ];
+        formParts.instance = responses[ 1 ].instance;
 
-    if ( formParts.form && formParts.model && formParts.instance ) {
-        gui.swapTheme( responses[ 0 ].theme || utils.getThemeFromFormStr( responses[ 0 ].form ) )
-            .then( function() {
+        if ( formParts.form && formParts.model && formParts.instance ) {
+            gui.swapTheme( responses[ 0 ].theme || utils.getThemeFromFormStr( responses[ 0 ].form ) )
+                .then( function() {
 
-                _init( formParts );
-            } )
-            .then( connection.getMaximumSubmissionSize )
-            .then( function( maxSize ) {
-                settings.maxSize = maxSize;
-            } );
-    } else {
-        throw new Error( t( 'error.unknown' ) );
-    }
-} ).catch( _showErrorOrAuthenticate );
+                    _init( formParts );
+                } )
+                .then( connection.getMaximumSubmissionSize )
+                .then( function( maxSize ) {
+                    settings.maxSize = maxSize;
+                } );
+        } else {
+            throw new Error( t( 'error.unknown' ) );
+        }
+    } ).catch( _showErrorOrAuthenticate );
 
 function _showErrorOrAuthenticate( error ) {
     $loader.addClass( 'fail' );

public/js/src/main-webform.js
@@ -10,7 +10,8 @@ var controller = require( './module/controller-webform' );
 var settings = require( './module/settings' );
 var connection = require( './module/connection' );
 var FormModel = require( 'enketo-core/src/js/Form-model' );
-var t = require( './module/translator' );
+var translator = require( './module/translator' );
+var t = translator.t;
 var store = require( './module/store' );
 var utils = require( './module/utils' );
 var formCache = require( './module/form-cache' );
@@ -30,7 +31,8 @@ _setEmergencyHandlers();
 
 if ( settings.offline ) {
     console.log( 'App in offline-capable mode.' );
-    formCache.init( survey )
+    translator.init( survey )
+        .then( formCache.init )
         .then( _addBranding )
         .then( _swapTheme )
         .then( _init )
@@ -45,7 +47,9 @@ if ( settings.offline ) {
         .catch( _showErrorOrAuthenticate );
 } else {
     console.log( 'App in online-only mode.' );
-    connection.getFormParts( survey )
+    translator.init( survey )
+        .then( connection.getFormParts )
+        .then( translator.init )
         .then( _addBranding )
         .then( _swapTheme )
         .then( _init )

commit e6291c991bbe9dfc6e453a4609cba5cd37d700fd

fixed: translation array values were discarded (regression after a0a9b489323be6924d76bafb4a602339bee8f7b6), closes https://github.com/kobotoolbox/enketo-express/issues/420

config/express.js
@@ -42,6 +42,7 @@ i18next
         //debug: true, // DEBUG
         whitelist: app.get( 'languages supported' ),
         fallbackLng: 'en',
+        joinArrays: '\n',
         backend: {
             loadPath: path.resolve( __dirname, '../locales/__lng__/translation.json' )
         },

public/js/src/module/gui.js
@@ -96,7 +96,6 @@ function setEventHandlers() {
     } );
 
     $( '.side-slider__app-version' ).on( 'click', function() {
-        console.debug( 'toggling' );
         $( '.side-slider__advanced' ).toggleClass( 'hide' );
     } );
 }

public/js/src/module/translator.js
@@ -34,6 +34,7 @@ init = function( something ) {
             .init( {
                 whitelist: settings.languagesSupported,
                 fallbackLng: 'en',
+                joinArrays: '\n',
                 backend: {
                     loadPath: '/locales/__lng__/translation.json',
                 },