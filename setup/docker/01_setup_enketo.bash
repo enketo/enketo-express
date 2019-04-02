f0a41f2f457fb83bb467d20d6e77459008c364fe
Olivier Léger <olivierleger@gmail.com>
2019-04-02 12:29:31 -0400

    Persist check files on disk

--- setup/docker/01_setup_enketo.bash
+++ setup/docker/01_setup_enketo.bash
@@ -8,12 +8,18 @@ cd ${ENKETO_SRC_DIR}/
 # Create a config. file if necessary.
 python setup/docker/create_config.py
 
-CONFIG_FILE_PATH="${ENKETO_SRC_DIR}/config/config.json"
-LAST_BUILD_COMMIT_FILE_PATH="${ENKETO_SRC_DIR}/.last_build_commit"
+CHECKSUM_DIR_PATH="${ENKETO_SRC_DIR}/checksum"
+
+CONFIG_FILE="config.json"
+CONFIG_FILE_PATH="${ENKETO_SRC_DIR}/config/${CONFIG_FILE}"
+SHA1SUM_CONFIG_FILE_PATH="${CHECKSUM_DIR_PATH}/${CONFIG_FILE}.sha1"
+
+LAST_BUILD_COMMIT_FILE="last_build_commit.txt"
+LAST_BUILD_COMMIT_FILE_PATH="${CHECKSUM_DIR_PATH}/${LAST_BUILD_COMMIT_FILE}"
 
 # Compare config version
 set +e  # Do not exit on `sha1sum` or `cat` error
-sha1sum -c "${CONFIG_FILE_PATH}.sha1"
+sha1sum -c "${SHA1SUM_CONFIG_FILE_PATH}"
 RUN_GRUNT=$([[ $? -eq 0 ]] && echo 0 || echo 1)
 
 # Compare commit version
@@ -29,5 +35,5 @@ if [ "$RUN_GRUNT" == 1 ]; then
     echo "Saving current commit..."
     echo $CURRENT_COMMIT > ${LAST_BUILD_COMMIT_FILE_PATH}
     echo "Saving config hash..."
-    sha1sum ${CONFIG_FILE_PATH} > "${CONFIG_FILE_PATH}.sha1"
+    sha1sum ${CONFIG_FILE_PATH} > "${SHA1SUM_CONFIG_FILE_PATH}"
 fi
