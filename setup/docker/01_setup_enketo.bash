66bfac16662bf8567c2b135d38f3d6b6019f3509
Olivier Léger <olivierleger@gmail.com>
2019-04-04 18:31:55 -0400

    Ensure grunt build files are present when checking if grunt is needed to be run

--- setup/docker/01_setup_enketo.bash
+++ setup/docker/01_setup_enketo.bash
@@ -26,11 +26,24 @@ CURRENT_COMMIT=$(git rev-parse HEAD) # Get current commit
 LAST_BUILD_COMMIT=$(cat ${LAST_BUILD_COMMIT_FILE_PATH}) || echo "-"
 RUN_GRUNT=$([[ "$RUN_GRUNT" == 0 && "$CURRENT_COMMIT" == "$LAST_BUILD_COMMIT" ]] && echo 0 || echo 1)
 
+# Ensure build files exist.
+# Can't test only on the parent folder because it's created by docker
+NOT_EMPTY_JS_PATH="public/js/build/.not-empty"
+NOT_EMPTY_CSS_PATH="public/css/.not-empty"
+NOT_EMPTY_LOCALES_PATH="locales/build/.not-empty"
+
+RUN_GRUNT=$([[ "$RUN_GRUNT" == 0 && -f "$NOT_EMPTY_JS_PATH" && -f "$NOT_EMPTY_CSS_PATH" && -f "$NOT_EMPTY_LOCALES_PATH" ]] && echo 0 || echo 1)
+
 if [ "$RUN_GRUNT" == 1 ]; then
     # Build.
+    echo "Grunt needs to be run!"
     grunt
     echo "Saving current commit..."
     echo $CURRENT_COMMIT > ${LAST_BUILD_COMMIT_FILE_PATH}
     echo "Saving config hash..."
     sha1sum ${CONFIG_FILE_PATH} > "${SHA1SUM_CONFIG_FILE_PATH}"
+    echo "Creating .not-empty files"
+    touch $NOT_EMPTY_JS_PATH
+    touch $NOT_EMPTY_CSS_PATH
+    touch $NOT_EMPTY_LOCALES_PATH
 fi
