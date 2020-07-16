1290838abbf958f754d837d9b3ec68aae00e2260
dbianco <dbianco@developmentgateway.org>
2020-07-16 15:21:39 -0300

    Fix copy of files

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 GENERATE_SOURCEMAP=false
-SRC="public/*.*"
-TARGET="/opt/enketo-express/public/ "
+SRC="public/"
+TARGET="/opt/enketo-express/public"
 
 if [ -f $SRC ]; then
-  rm -R /opt/enketo-express/public
+  rm -Rf /opt/enketo-express/public
   cp -R $SRC $TARGET
   sudo systemctl restart enketo
 else
