f3b297f45ece291c1968552fc29a38a7e35af50e
dbianco <dbianco@developmentgateway.org>
2020-07-21 12:59:59 -0300

    Add deploy file

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+GENERATE_SOURCEMAP=false
+SRC="./public"
+TARGET="/opt/enketo-express/public"
+
+if [ -d $SRC ]; then
+  rm -Rf /opt/enketo-express/public
+  cp -R $SRC $TARGET
+  sudo systemctl restart enketo
+else
+  echo "Dir not found: $SRC"
+  exit 1
+fi
