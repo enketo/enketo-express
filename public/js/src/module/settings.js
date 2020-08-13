c705c6d9b026c537db29f181754a1143d9a341fa
Hélène Martin <lognaturel@gmail.com>
2020-08-13 12:51:28 -0700

    fixed: identify form type prefix when Enketo at subpath (#197)

--- public/js/src/module/settings.js
+++ public/js/src/module/settings.js
@@ -63,13 +63,13 @@ settings.languageOverrideParameter = queryParams.lang ? {
 settings.maxSize = DEFAULT_MAX_SIZE;
 
 // add type
-if ( window.location.pathname.indexOf( '/preview' ) === 0 ) {
+if ( window.location.pathname.includes( '/preview/' ) || window.location.pathname.endsWith('/preview') ) {
     settings.type = 'preview';
-} else if ( window.location.pathname.indexOf( '/single' ) === 0 ) {
+} else if ( window.location.pathname.includes( '/single/' ) ) {
     settings.type = 'single';
-} else if ( window.location.pathname.indexOf( '/edit' ) === 0 ) {
+} else if ( window.location.pathname.includes( '/edit/' ) ) {
     settings.type = 'edit';
-} else if ( window.location.pathname.indexOf( '/view' ) === 0 ) {
+} else if ( window.location.pathname.includes( '/view/' ) ) {
     settings.type = 'view';
 } else {
     settings.type = 'other';
