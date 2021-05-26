63641a24c5f8d39143da37e9c3532fbdd21e5f34
dbianco <dbianco@developmentgateway.org>
2021-05-26 00:07:13 -0300

    Disable fields

--- app/views/surveys/component/disable-fields.js
+++ app/views/surveys/component/disable-fields.js
@@ -1,4 +1,14 @@
-var inputs = document.getElementsByClassName(); 
+var inputs = document.getElementsByTagName("input"); 
 for (var i = 0; i < inputs.length; i++) { 
-    inputs[i].disabled = true; 
+    inputs[i].disabled = true;
+}
+
+var textarea = document.getElementsByTagName("textarea"); 
+for (var i = 0; i < textarea.length; i++) { 
+    textarea[i].disabled = true;
+} 
+
+var button = document.getElementsByTagName("button"); 
+for (var i = 0; i < button.length; i++) { 
+    button[i].disabled = true;
 } 
