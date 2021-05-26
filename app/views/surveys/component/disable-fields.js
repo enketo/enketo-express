aa7ba0b8a4e0e4d7150b9fa516b88b33e3400cef
dbianco <dbianco@developmentgateway.org>
2021-05-26 09:02:58 -0300

    Add window.onload for disabling elements

--- app/views/surveys/component/disable-fields.js
+++ app/views/surveys/component/disable-fields.js
@@ -1,14 +1,17 @@
-var inputs = document.getElementsByTagName("input"); 
-for (var i = 0; i < inputs.length; i++) { 
-    inputs[i].disabled = true;
+window.onload = 
+function myFunction() {
+    var inputs = document.getElementsByTagName("input"); 
+    for (var i = 0; i < inputs.length; i++) { 
+        inputs[i].disabled = true;
+    }
+
+    var textarea = document.getElementsByTagName("textarea"); 
+    for (var i = 0; i < textarea.length; i++) { 
+        textarea[i].disabled = true;
+    } 
+
+    var button = document.getElementsByTagName("button"); 
+    for (var i = 0; i < button.length; i++) { 
+        button[i].disabled = true;
+    } 
 }
-
-var textarea = document.getElementsByTagName("textarea"); 
-for (var i = 0; i < textarea.length; i++) { 
-    textarea[i].disabled = true;
-} 
-
-var button = document.getElementsByTagName("button"); 
-for (var i = 0; i < button.length; i++) { 
-    button[i].disabled = true;
-} 
