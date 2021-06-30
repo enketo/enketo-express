enketo/enketo-express

commit 9b8891a9aacd9605222cec3aad9a7ece0ce43aa9

Support for jr:instance/last-saved endpoint (#269)
* Pin Node version with Volta

* Initial form-cache implementation of get/set last-saved

* Don't set last saved records when encrypted

* Expose `save` function from records-queue

This replaces the dynamic function call in controller-webform, making it a little easier to find the call site. If at some point in the future we migrate to TypeScript this would be somewhat less necessary.

Another option, discussed with @lognaturel, would be to remove this logic from controller-webform and have records-queue determine which is appropriate. I agree this would be an improvement, but I think that's probably out of scope for the last-saved work.

* Implement/test offline support for last-saved…

This includes a refactor of the encryption exception, ensuring Surveys with encryption enabled do not store unencrypted draft Records. This refactor was accomplished by persisting encryption-enabled state on the Survey itself.

* Support online mode, populate last-saved secondary instances

* Correct typo

* Initial changes for feedback on PR #269

- Corrected assignment to `binaryDefaults`
- Corrected a typo
- Removed references to last-saved where no longer relevant

* Move tests related to last-saved to test/client/feature/last-saved.spec.js

* Move tests related to encryption-enabled surveys/last-saved records to test/client/feature/survey-encryption.spec.js

* Document fallback to a survey's model when it has no last-saved record

* Add AbortController polyfill for old Chrome version in CI

* Don't store last saved records for surveys without a last-saved secondary instance

* Polyfill Object.fromEntries...

* Blob.text -> Response.text to hopefully address CI failure

* Fix overwrite of lastSavedRecord on first online submit, edit regression

* Move remaining last-saved tests to the feature

* Fix `npm run build-docs`

* Use, and fix, getDisplayableRecordList

public/js/src/module/connection.js
@@ -2,10 +2,54 @@
  * Deals with communication to the server (in process of being transformed to using Promises)
  */
 
+import encryptor from './encryptor';
+import formCache from './form-cache';
 import settings from './settings';
 import { t } from './translator';
 import utils from './utils';
+
+/**
+ * @typedef {import('../../../../app/models/record-model').EnketoRecord} EnketoRecord
+ */
+
+/**
+ * @typedef {import('../../../../app/models/survey-model').SurveyObject} Survey
+ */
+
+/**
+ * @typedef {import('../../../../app/models/survey-model').SurveyExternalData} SurveyExternalData
+ */
+
+/**
+ * @typedef BatchPrepped
+ * @property { string } instanceId
+ * @property { string } deprecatedId
+ * @property { FormData } formData
+ * @property { string[] } failedFiles
+ */
+
+/**
+ * @typedef UploadRecordOptions
+ * @property { boolean } [isLastSaved]
+ */
+
+/**
+ * @typedef UploadBatchResult
+ * @property { number } status
+ * @property { Array<string | undefined> } failedFiles
+ * @property { string } [message]
+ */
+
+/**
+ * @typedef GetFormPartsProps
+ * @property { string } enketoId
+ * @property { Record<string, string> } [defaults]
+ * @property { string } [instanceId]
+ * @property { string } [xformUrl]
+ */
+
 const parser = new DOMParser();
+const xmlSerializer = new XMLSerializer();
 const CONNECTION_URL = `${settings.basePath}/connection`;
 const TRANSFORM_URL = `${settings.basePath}/transform/xform${settings.enketoId ? `/${settings.enketoId}` : ''}`;
 const TRANSFORM_HASH_URL = `${settings.basePath}/transform/xform/hash/${settings.enketoId}`;
@@ -29,13 +73,14 @@ function getOnlineStatus() {
         .catch( () => false );
 }
 
-/*
+/**
  * Uploads a complete record
  *
- * @param  {{xml: string, files: [File]}} record
- * @return { Promise }
+ * @param  { EnketoRecord } record
+ * @param  { UploadRecordOptions } [options]
+ * @return { Promise<UploadBatchResult> }
  */
-function uploadRecord( record ) {
+function uploadRecord( record, options = {} ) {
     let batches;
 
     try {
@@ -44,26 +89,40 @@ function uploadRecord( record ) {
         return Promise.reject( e );
     }
 
-    batches.forEach( batch => {
-        batch.instanceId = record.instanceId;
-        batch.deprecatedId = record.deprecatedId;
-    } );
+    /** @type { Promise<UploadBatchResult[]> } */
+    let resultsPromise = Promise.resolve( [] );
+
+    /** @type { UploadBatchResult } */
+    let result;
 
     // Perform batch uploads sequentially for to avoid issues when connections are very poor and
     // a serious issue with ODK Aggregate (https://github.com/kobotoolbox/enketo-express/issues/400)
-    return batches.reduce( ( prevPromise, batch ) => prevPromise.then( () => _uploadBatch( batch ) ), Promise.resolve() )
+    return batches.reduce( ( prevPromise, batch ) => {
+        return prevPromise.then( results => {
+            return _uploadBatch( batch ).then( result => {
+                results.push( result );
+
+                return results;
+            } );
+        } );
+    }, resultsPromise )
         .then( results => {
             console.log( 'results of all batches submitted', results );
 
-            return results[ 0 ];
-        } );
+            result = results[ 0 ];
+
+            if ( options.isLastSaved ) {
+                return formCache.setLastSavedRecord( record.enketoId, record );
+            }
+        } )
+        .then( () => result );
 }
 
 /**
  * Uploads a single batch of a single record.
  *
- * @param {{formData: FormData, failedFiles: [string]}} recordBatch - formData object to send
- * @return { Promise }      [description]
+ * @param { BatchPrepped } recordBatch - formData object to send
+ * @return { Promise<UploadBatchResult> }      [description]
  */
 function _uploadBatch( recordBatch ) {
     // Submission URL is dynamic, because settings.submissionParameter only gets populated after loading form from
@@ -87,7 +146,8 @@ function _uploadBatch( recordBatch ) {
         body: recordBatch.formData
     } )
         .then( response => {
-            const result = {
+            /** @type { UploadBatchResult } */
+            let result = {
                 status: response.status,
                 failedFiles: ( recordBatch.failedFiles ) ? recordBatch.failedFiles : undefined
             };
@@ -123,27 +183,36 @@ function _uploadBatch( recordBatch ) {
 /**
  * Builds up a record array including media files, divided into batches
  *
- * @param { { name: string, data: string } } record - record object
+ * @param { EnketoRecord } record - record object
+ * @return { BatchPrepped[] }
  */
 function _prepareFormDataArray( record ) {
     const recordDoc = parser.parseFromString( record.xml, 'text/xml' );
+
+    /** @type {Array<Omit<HTMLInputElement, 'type'>>} */
     const fileElements = Array.prototype.slice.call( recordDoc.querySelectorAll( '[type="file"]' ) ).map( el => {
         el.removeAttribute( 'type' );
 
         return el;
     } );
-    const xmlData = new XMLSerializer().serializeToString( recordDoc.documentElement );
+    const xmlData = xmlSerializer.serializeToString( recordDoc.documentElement );
     const xmlSubmissionBlob = new Blob( [ xmlData ], {
         type: 'text/xml'
     } );
     const availableFiles = record.files || [];
     const sizes = [];
-    const failedFiles = [];
+
+    /** @type {string[]} */
+    let failedFiles = [];
+
     const submissionFiles = [];
     let batches = [
         []
     ];
-    const batchesPrepped = [];
+
+    /** @type {BatchPrepped[]} */
+    let batchesPrepped = [];
+
     const maxSize = settings.maxSize;
 
     fileElements.forEach( el => {
@@ -182,7 +251,6 @@ function _prepareFormDataArray( record ) {
     console.log( `splitting record into ${batches.length} batches to reduce submission size `, batches );
 
     batches.forEach( batch => {
-        let batchPrepped;
         const fd = new FormData();
 
         fd.append( 'xml_submission_file', xmlSubmissionBlob, 'xml_submission_file' );
@@ -190,7 +258,9 @@ function _prepareFormDataArray( record ) {
         if ( csrfToken ) fd.append( '__csrf', csrfToken );
 
         // batch with XML data
-        batchPrepped = {
+        let batchPrepped = {
+            instanceId: record.instanceId,
+            deprecatedId: record.deprecatedId,
             formData: fd,
             failedFiles
         };
@@ -281,20 +351,48 @@ function getMaximumSubmissionSize( survey ) {
 /**
  * Obtains HTML Form, XML Model and External Instances
  *
- * @param { object } props - form properties object
- * @return { Promise } a Promise that resolves with a form parts object
+ * @param { GetFormPartsProps } props - form properties object
+ * @return { Promise<Survey> } a Promise that resolves with a form parts object
  */
 function getFormParts( props ) {
+    const isEditing = props.instanceId != null;
+
+    /** @type {Survey} */
+    let survey;
+
+    /** @type {XMLDocument} */
+    let model;
+
+    /** @type {EnketoRecord | undefined} */
+    let lastSavedRecord;
 
     return _postData( TRANSFORM_URL + _getQuery(), {
         xformUrl: props.xformUrl
     } )
         .then( data => {
-            data.enketoId = props.enketoId;
-            data.theme = data.theme || utils.getThemeFromFormStr( data.form ) || settings.defaultTheme;
+            model = parser.parseFromString( data.model, 'text/xml' );
 
-            return _getExternalData( data );
-        } );
+            const encryptedSubmission = model.querySelector( 'submission[base64RsaPublicKey]' );
+
+            survey = Object.assign( {}, data, {
+                enketoId: props.enketoId,
+                theme: data.theme || utils.getThemeFromFormStr( data.form ) || settings.defaultTheme,
+            } );
+
+            if ( encryptedSubmission != null ) {
+                survey = encryptor.setEncryptionEnabled( survey );
+            }
+
+            if ( !isEditing ) {
+                return formCache.getLastSavedRecord( props.enketoId );
+            }
+        } )
+        .then( lastSaved => {
+            lastSavedRecord = lastSaved;
+
+            return _getExternalData( survey, model, lastSavedRecord );
+        } )
+        .then( externalData => Object.assign( survey, { externalData, lastSavedRecord } ) );
 }
 
 function _postData( url, data = {}  ){
@@ -332,6 +430,10 @@ function _request( url, method = 'POST', data = {}  ){
         } );
 }
 
+/**
+ * @param { Response } response
+ * @return { Response }
+ */
 function _throwResponseError( response ){
     if ( !response.ok ){
         return response.json()
@@ -356,28 +458,58 @@ function _encodeFormData( data ){
         .join( '&' );
 }
 
-function _getExternalData( survey ) {
-    const tasks = [];
+/**
+ * Gets a survey's last-saved record data as an `XMLDocument`, defaulting to
+ * the survey's model when no last-saved record exists.
+ *
+ * @param {Document} model
+ * @param {EnketoRecord} [lastSavedRecord]
+ * @return {XMLDocument}
+ */
+function _getLastSavedInstance( model, lastSavedRecord ) {
+    if ( lastSavedRecord == null ) {
+        const modelDefault = model.querySelector( 'model > instance > *' ).cloneNode( true );
+
+        let doc = document.implementation.createDocument( null, '', null );
+
+        doc.appendChild( modelDefault );
+
+        return doc;
+    } else {
+        return parser.parseFromString( lastSavedRecord.xml, 'text/xml' );
+    }
+}
+
+/**
+ * @param {Survey} survey
+ * @param {Document} model
+ * @param {EnketoRecord} [lastSavedRecord]
+ * @return {Promise<SurveyExternalData[]>}
+ */
+function _getExternalData( survey, model, lastSavedRecord ) {
+    /** @type {Array<Promise<SurveyExternalData>>} */
+    let tasks = [];
 
     try {
-        const doc = parser.parseFromString( survey.model, 'text/xml' );
-
-        survey.externalData = [ ...doc.querySelectorAll ( 'instance[id][src]' ) ]
+        const externalInstances = [ ...model.querySelectorAll( 'instance[id][src]' ) ]
             .map( instance => ( {
                 id:  instance.id,
                 src: instance.getAttribute( 'src' )
             } ) );
 
-        survey.externalData
+        externalInstances
             .forEach( ( instance, index ) => {
-                tasks.push( _getDataFile( instance.src, survey.languageMap )
-                    .then( xmlData => {
-                        instance.xml = xmlData;
+                /** @type {Promise<SurveyExternalData>} */
+                const request = instance.src === formCache.LAST_SAVED_VIRTUAL_ENDPOINT
+                    ? Promise.resolve( _getLastSavedInstance( model, lastSavedRecord ) )
+                    : _getDataFile( instance.src, survey.languageMap );
 
-                        return instance;
+                tasks.push( request
+                    .then( xmlData => {
+                        return Object.assign( {}, instance, { xml: xmlData } );
                     } )
                     .catch( e => {
-                        survey.externalData.splice( index, 1 );
+                        tasks.splice( index, 1 );
                         // let external data files fail quietly in previews with ?form= parameter
                         if ( !survey.enketoId ){
                             return;
@@ -390,8 +522,7 @@ function _getExternalData( survey ) {
         return Promise.reject( e );
     }
 
-    return Promise.all( tasks )
-        .then( () => survey );
+    return Promise.all( tasks );
 }
 
 

public/js/src/module/form-cache.js
@@ -7,9 +7,28 @@ import events from './event';
 import settings from './settings';
 import connection from './connection';
 import assign from 'lodash/assign';
+import encryptor from './encryptor';
+
+/**
+ * @typedef {import('../../../../app/models/record-model').EnketoRecord} EnketoRecord
+ */
+
+/**
+ * @typedef {import('../../../../app/models/survey-model').SurveyObject} Survey
+ */
+
+const CACHE_UPDATE_INITIAL_DELAY = 3 * 1000;
+const CACHE_UPDATE_INTERVAL = 20 * 60 * 1000;
+const LAST_SAVED_VIRTUAL_ENDPOINT = 'jr://instance/last-saved';
+
+const parser = new DOMParser();
 
 let hash;
 
+/**
+ * @param {Survey} survey
+ * @return {Promise<Survey>}
+ */
 function init( survey ) {
     return store.init()
         .then( () => get( survey ) )
@@ -21,14 +40,21 @@ function init( survey ) {
             }
         } )
         .then( _processDynamicData )
-        .then( _setUpdateIntervals )
-        .then( _setResetListener );
+        .then( _setUpdateIntervals );
 }
 
+/**
+ * @param {Survey} survey
+ * @return Survey
+ */
 function get( survey ) {
     return store.survey.get( survey.enketoId );
 }
 
+/**
+ * @param {Survey} survey
+ * @return {Promise<Survey>}
+ */
 function set( survey ) {
     return connection.getFormParts( survey )
         .then( _swapMediaSrc )
@@ -36,10 +62,72 @@ function set( survey ) {
         .then( store.survey.set );
 }
 
+/**
+ *
+ * @param { string } enketoId
+ * @return { Promise<EnketoRecord | undefined> }
+ */
+function getLastSavedRecord( enketoId ) {
+    return store.survey.get( enketoId )
+        .then( survey => {
+            if ( survey != null ) {
+                return survey.lastSavedRecord;
+            }
+        } );
+}
+
+/**
+ * @param {Survey} survey
+ */
+function _isLastSaveEnabled( survey ) {
+    return (
+        survey.externalData != null &&
+        survey.externalData.find( data => data.src === LAST_SAVED_VIRTUAL_ENDPOINT ) != null &&
+        !encryptor.isEncryptionEnabled( survey )
+    );
+}
+
+/**
+ *
+ * @param { string } enketoId
+ * @param { EnketoRecord } lastSavedRecord
+ * @return { Promise<Survey> }
+ */
+function setLastSavedRecord( enketoId, lastSavedRecord ) {
+    return store.survey.get( enketoId )
+        .then( survey => {
+            if ( !_isLastSaveEnabled( survey ) ) {
+                return Promise.resolve( survey );
+            }
+
+            if ( Array.isArray( survey.externalData ) ) {
+                const { xml } = lastSavedRecord;
+
+                survey.externalData.forEach( item => {
+                    if ( item.src === LAST_SAVED_VIRTUAL_ENDPOINT ) {
+                        item.xml = parser.parseFromString( xml, 'text/xml' );
+                    }
+                } );
+            }
+
+            const update = Object.assign( {}, survey, { lastSavedRecord } );
+
+            return store.survey.update( update );
+        } );
+}
+
+/**
+ * @param {Survey} survey
+ * @return {Promise<void>}
+ */
 function remove( survey ) {
     return store.survey.remove( survey.enketoId );
 }
 
+/**
+ * @param {Survey} survey
+ * @return {Promise<Survey>}
+ */
 function _processDynamicData( survey ) {
     // TODO: In the future this method could perhaps be used to also store
     // dynamic defaults. However, the issue would be to figure out how to clear
@@ -93,6 +181,10 @@ function _processDynamicData( survey ) {
         .then( () => survey );
 }
 
+/**
+ * @param {Survey} survey
+ * @return {Promise<Survey>}
+ */
 function _setUpdateIntervals( survey ) {
     hash = survey.hash;
 
@@ -102,29 +194,11 @@ function _setUpdateIntervals( survey ) {
     // that open the form right after the XForm update.
     setTimeout( () => {
         _updateCache( survey );
-    }, 3 * 1000 );
+    }, CACHE_UPDATE_INITIAL_DELAY );
     // check for form update every 20 minutes
     setInterval( () => {
         _updateCache( survey );
-    }, 20 * 60 * 1000 );
-
-    return Promise.resolve( survey );
-}
-
-/**
- * Form resets require reloading the form media.
- * This makes form resets slower, but it makes initial form loads faster.
- *
- * @param { object } survey - [description]
- */
-function _setResetListener( survey ) {
-
-    document.addEventListener( events.FormReset().type, event => {
-        if ( event.target.nodeName.toLowerCase() === 'form' ) {
-            survey.htmlView = event.target;
-            updateMedia( survey );
-        }
-    } );
+    }, CACHE_UPDATE_INTERVAL );
 
     return Promise.resolve( survey );
 }
@@ -132,11 +206,12 @@ function _setResetListener( survey ) {
 /**
  * Handles loading form media for newly added repeats.
  *
- * @param { object } survey - [description]
+ * @param { Survey } survey - survey object
+ * @return { Promise<Survey> }
  */
 function _setRepeatListener( survey ) {
     //Instantiate only once, after loadMedia has been completed (once)
-    survey.htmlView.addEventListener( events.AddRepeat().type, event => {
+    document.querySelector( 'form.or' ).addEventListener( events.AddRepeat().type, event => {
         _loadMedia( survey, [ event.target ] );
     } );
 
@@ -147,7 +222,8 @@ function _setRepeatListener( survey ) {
  * Changes src attributes in view to data-offline-src to facilitate loading those resources
  * from the browser storage.
  *
- * @param { object } survey - survey object
+ * @param { Survey } survey - survey object
+ * @return { Promise<Survey> }
  */
 function _swapMediaSrc( survey ) {
     survey.form = survey.form.replace( /(src="[^"]*")/g, 'data-offline-$1 src=""' );
@@ -160,7 +236,8 @@ function _swapMediaSrc( survey ) {
  * Loads all default binary files and adds them to the survey object. It removes the src
  * attributes from model nodes with default binary files.
  *
- * @param { object } survey - survey object
+ * @param { Survey } survey - survey object
+ * @return { Promise<Survey> }
  */
 function _addBinaryDefaultsAndUpdateModel( survey ) {
     // The mechanism for default binary files is as follows:
@@ -203,8 +280,8 @@ function _addBinaryDefaultsAndUpdateModel( survey ) {
  * If the form/data server updates their max size setting, this value
  * will be updated the next time the cache is refreshed.
  *
- * @param  { object } survey - [description]
- * @return { object }        [description]
+ * @param { Survey } survey - survey object
+ * @return { Promise<Survey> }
  */
 function updateMaxSubmissionSize( survey ) {
 
@@ -222,15 +299,15 @@ function updateMaxSubmissionSize( survey ) {
                 return survey;
             } );
     } else {
-        return survey;
+        return Promise.resolve( survey );
     }
 }
 
 /**
  * Loads survey resources either from the store or via HTTP (and stores them).
  *
- * @param  { object } survey - [description]
- * @return { Promise }        [description]
+ * @param { Survey } survey - survey object
+ * @return { Promise<Survey> }
  */
 function updateMedia( survey ) {
     const requests = [];
@@ -239,7 +316,7 @@ function updateMedia( survey ) {
         return _loadMedia( survey )
             .then( _setRepeatListener );
     }
-    const containers = [ survey.htmlView ];
+    const containers = [ document.querySelector( 'form.or' ) ];
     const formHeader = document.querySelector( '.form-header' );
     if ( formHeader ) {
         containers.push( formHeader );
@@ -294,7 +371,7 @@ function _loadMedia( survey, targetContainers ) {
     const URL = window.URL || window.webkitURL;
 
     if ( !targetContainers ) {
-        targetContainers = [ survey.htmlView ];
+        targetContainers = [ document.querySelector( 'form.or' ) ];
         const formHeader = document.querySelector( '.form-header' );
         if ( formHeader ) {
             targetContainers.push( formHeader );
@@ -371,6 +448,10 @@ function _updateCache( survey ) {
                         // media will be updated next time the form is loaded if resources is undefined
                         formParts.resources = undefined;
 
+                        if ( !_isLastSaveEnabled( formParts ) ) {
+                            delete formParts.lastSavedRecord;
+                        }
+
                         return formParts;
                     } )
                     .then( _swapMediaSrc )
@@ -422,5 +503,10 @@ export default {
     updateMaxSubmissionSize,
     updateMedia,
     remove,
-    flush
+    flush,
+    getLastSavedRecord,
+    setLastSavedRecord,
+    CACHE_UPDATE_INITIAL_DELAY,
+    CACHE_UPDATE_INTERVAL,
+    LAST_SAVED_VIRTUAL_ENDPOINT,
 };

commit 20f78b7f44b4b5d21fad62dfcc74dae8121a3a42

Fixed: Form preview fails with IDBObjectStore error
Fixes #279.

This introduces two changes to fix a regression introduced in #269:

- The store is bypassed in initialization when a form is previewed. As noted in a code comment, this was implemented in form-cache.js primarily to enable testing, but likely more appropriate in enketo-webform.js
- `getFormParts` now only calls `getLastSavedRecord` when `settings.type` is `other`, which is the default value when filling a form (versus `preview`, `single`, `edit`, `view`)

public/js/src/module/connection.js
@@ -355,7 +355,7 @@ function getMaximumSubmissionSize( survey ) {
  * @return { Promise<Survey> } a Promise that resolves with a form parts object
  */
 function getFormParts( props ) {
-    const isEditing = props.instanceId != null;
+    const isLastSavedEnabled = settings.type === 'other';
 
     /** @type {Survey} */
     let survey;
@@ -383,7 +383,7 @@ function getFormParts( props ) {
                 survey = encryptor.setEncryptionEnabled( survey );
             }
 
-            if ( !isEditing ) {
+            if ( isLastSavedEnabled ) {
                 return formCache.getLastSavedRecord( props.enketoId );
             }
         } )

public/js/src/module/form-cache.js
@@ -30,6 +30,14 @@ let hash;
  * @return {Promise<Survey>}
  */
 function init( survey ) {
+    /**
+     * Note: it would probably be better if this were in enketo-webform.js, as it
+     * doesn't relate to the form cache per se. It's included here for testability.
+     */
+    if ( settings.type === 'preview' ) {
+        return connection.getFormParts( survey );
+    }
+
     return store.init()
         .then( () => get( survey ) )
         .then( result => {

test/client/feature/last-saved.spec.js
@@ -640,7 +640,14 @@ describe( 'Support for jr://instance/last-saved endpoint', () => {
         const defaultInstanceData = '<data id="modelA"><item>initial</item><meta><instanceID/></meta></data>';
         const xmlSerializer = new XMLSerializer();
 
+        /** @type {string} */
+        let settingsType;
+
         beforeEach( () => {
+            settingsType = 'other';
+
+            sandbox.stub( settings, 'type' ).get( () => settingsType );
+
             sandbox.stub( settings, 'enketoId' ).get( () => enketoId );
 
             sandbox.stub( window, 'fetch' ).callsFake( () => {
@@ -723,6 +730,8 @@ describe( 'Support for jr://instance/last-saved endpoint', () => {
         } );
 
         it( 'populates a last-saved secondary instance with the model\'s defaults when editing an instance', done => {
+            settingsType = 'edit';
+
             const lastSavedRecord = {
                 enketoId,
                 instanceId,
@@ -733,6 +742,26 @@ describe( 'Support for jr://instance/last-saved endpoint', () => {
 
             formCache.setLastSavedRecord( enketoId, lastSavedRecord )
                 .then( () => connection.getFormParts( { enketoId, instanceId } ) )
+                .then( result => {
+                    expect( Array.isArray( result.externalData ) ).to.equal( true );
+                    expect( result.externalData.length ).to.equal( 1 );
+
+                    const data = result.externalData[0];
+
+                    expect( data.id ).to.equal( 'last-saved' );
+                    expect( data.src ).to.equal( 'jr://instance/last-saved' );
+
+                    const xml = xmlSerializer.serializeToString( data.xml.documentElement, 'text/xml' );
+
+                    expect( xml ).to.equal( defaultInstanceData );
+                } )
+                .then( done, done );
+        } );
+
+        it( 'populates a last-saved secondary instance with the model\'s defaults when previewing a form', done => {
+            settingsType = 'preview';
+
+            connection.getFormParts( { enketoId, instanceId } )
                 .then( result => {
                     expect( Array.isArray( result.externalData ) ).to.equal( true );
                     expect( result.externalData.length ).to.equal( 1 );

test/client/form-cache.spec.js
@@ -1,6 +1,7 @@
 import formCache from '../../public/js/src/module/form-cache';
 import connection from '../../public/js/src/module/connection';
 import store from '../../public/js/src/module/store';
+import settings from '../../public/js/src/module/settings';
 
 /**
  * @typedef { import('sinon').SinonSandbox } SinonSandbox
@@ -92,7 +93,12 @@ describe( 'Client Form Cache', () => {
         };
 
         getFormPartsSpy = sandbox.stub( connection, 'getFormParts' ).callsFake( survey => {
-            return formCache.getLastSavedRecord( survey.enketoId )
+            return Promise.resolve( survey.enketoId )
+                .then( enketoId => {
+                    if ( enketoId != null ) {
+                        return formCache.getLastSavedRecord( survey.enketoId );
+                    }
+                } )
                 .then( lastSavedRecord => {
                     if ( lastSavedRecord != null ) {
                         return { lastSavedRecord };
@@ -188,5 +194,32 @@ describe( 'Client Form Cache', () => {
                 .then( done, done );
         } );
 
+    } );
+
+    describe( 'previews', () => {
+        beforeEach( () => {
+            sandbox.stub( settings, 'type' ).get( () => 'preview' );
+        } );
+
+        it( 'bypasses the store when initializing a form in preview mode', done => {
+            const previewSurvey = {
+                enketoId: null,
+                xformUrl: 'https://xlsform.getodk.org/downloads/b0x0gdti/Range%20test.xml',
+                defaults: {}
+            };
+
+            let initResult;
+
+            formCache.init( previewSurvey )
+                .then( survey => {
+                    initResult = survey;
+
+                    return getFormPartsSpy.getCall( 0 ).returnValue;
+                } )
+                .then( formParts => {
+                    expect( initResult ).to.deep.equal( formParts );
+                } )
+                .then( done, done );
+        } );
     } );
 } );