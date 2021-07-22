ca75e8eeb9481eddb695538949beaf419843a083
eyelidlessness <eyelidlessness@users.noreply.github.com>
2021-07-22 12:39:09 -0700

    Switch build from rollup to esbuild (#289)
    - Moves /build/index.html to /src to properly include in build step and ignore /build
    - Like enketo/enketo-express#289, switches from Rollup to esbuild for consistency and improved build performance

--- buildFiles.js
+++
@@ -1,8 +0,0 @@
-const pkg = require( './package' );
-const entries = pkg.entries;
-const bundles = entries.map( file => file.replace( '/src/', '/build/' ).replace( '.js', '-bundle.js' ) );
-
-module.exports = {
-    entries,
-    bundles
-};
