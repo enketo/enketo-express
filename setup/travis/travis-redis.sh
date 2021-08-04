2e6ff1287eb0f5c557d8eaf1a35082cd8b0afafc
eyelidlessness <gnosis@gmail.com>
2021-08-04 15:37:56 -0700

    Turns out the Vagrant setup already uses systemd, let's learn from that

--- setup/travis/travis-redis.sh
+++ setup/travis/travis-redis.sh
@@ -2,34 +2,16 @@
 
 echo "Setting up Redis instances on ports 6379 and 6380..."
 
+# Based on setup/vagrant/bootstrap.sh
 
-# Based on https://learn.jetrails.com/article/multiple-redis-servers-with-systemd
-
-# Disable the default Redis service
-
-sudo systemctl stop redis-server
-sudo systemctl disable redis-server
-
-# Redis config
+systemctl stop redis
+systemctl disable redis
+systemctl daemon-reload
 
 REDIS_SOURCE=${TRAVIS_BUILD_DIR}/setup/redis/conf
-REDIS_TARGET=/etc/redis
-sudo cp $REDIS_SOURCE/redis-enketo-cache.conf $REDIS_TARGET/redis-enketo-cache.conf
-sudo cp $REDIS_SOURCE/redis-enketo-main.conf $REDIS_TARGET/redis-enketo-main.conf
 
-# Systemd config
+cp -f $REDIS_SOURCE/redis-enketo-main.conf /etc/redis/
+cp -f $REDIS_SOURCE/redis-enketo-cache.conf /etc/redis/
 
-SYSTEMD_SOURCE=${TRAVIS_BUILD_DIR}/setup/travis
-SYSTEMD_TARGET=/lib/systemd/system
-sudo cp $SYSTEMD_SOURCE/redis-enketo-cache.service $SYSTEMD_TARGET/redis-enketo-cache.service
-sudo cp $SYSTEMD_SOURCE/redis-enketo-main.service $SYSTEMD_TARGET/redis-enketo-main.service
-
-# Enable and start Redis services
-
-sudo systemctl enable redis-enketo-cache.service redis-enketo-main.service
-sudo systemctl start redis-enketo-cache.service redis-enketo-main.service
-sudo journalctl -xe
-
-# Wait for Redis services to start
-
-sleep 3
+sudo systemctl enable redis-server@enketo-main.service redis-server@enketo-cache.service
+sudo systemctl start redis-server@enketo-main.service redis-server@enketo-cache.service
