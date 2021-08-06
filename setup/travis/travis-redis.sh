d52d3025f0ab4f625bbf8741102e905f3dbc86b8
eyelidlessness <gnosis@gmail.com>
2021-08-06 10:32:39 -0700

    Start Redis unsupervised
    We may need to install Redis with the service removed, will see!

--- setup/travis/travis-redis.sh
+++ setup/travis/travis-redis.sh
@@ -2,59 +2,12 @@
 
 echo "Setting up Redis instances on ports 6379 and 6380..."
 
-# echo "default redis conf"
-# sudo cat /etc/redis/redis.conf
+# Start Redis instances with Enketo configs, overriding `supervised systemd`
 
-# echo "\n\ndefault redis service"
-# cat /lib/systemd/system/redis-server.service
+CONFIG_DIR=${TRAVIS_BUILD_DIR}/setup/redis/conf
 
-# Based on https://learn.jetrails.com/article/multiple-redis-servers-with-systemd
-
-# Disable the default Redis service
-
-sudo systemctl stop redis-server
-sudo systemctl disable redis-server
-sudo mkdir -p /etc/redis/{redis-server.pre-up.d,redis-server.post-down.d,redis-server.post-up.d,redis-server.pre-down.d}
-sudo mkdir -p /var/lib/{redis-cache,redis-sessions}
-
-# Redis config
-
-REDIS_SOURCE=${TRAVIS_BUILD_DIR}/setup/redis/conf
-REDIS_TARGET=/etc/redis
-
-sudo cat $REDIS_SOURCE/redis-enketo-cache.conf | grep -v "^supervised systemd" > $REDIS_SOURCE/redis-travis-cache.conf
-sudo cp -f $REDIS_SOURCE/redis-travis-cache.conf $REDIS_TARGET/enketo-cache.conf
-
-sudo cat $REDIS_SOURCE/redis-enketo-main.conf | grep -v "^supervised systemd" > $REDIS_SOURCE/redis-travis-main.conf
-sudo cp -f $REDIS_SOURCE/redis-travis-main.conf $REDIS_TARGET/enketo-main.conf
-
-# Systemd config
-
-SYSTEMD_SOURCE=${TRAVIS_BUILD_DIR}/setup/redis/systemd
-SYSTEMD_TARGET=/lib/systemd/system
-
-sudo cp $SYSTEMD_SOURCE/redis-enketo-cache.service $SYSTEMD_TARGET/redis-enketo-cache.service
-sudo cp $SYSTEMD_SOURCE/redis-enketo-main.service $SYSTEMD_TARGET/redis-enketo-main.service
-
-# echo "Starting up outside Systemd service"
-# sudo /usr/bin/redis-server /etc/redis/enketo-cache.conf --supervised no --daemonize yes
-# echo "Ping??"
-# redis-cli -p 6380 ping
-# redis-cli -p 6380 shutdown
-
-# Enable and start Redis services
-
-sudo systemctl enable redis-enketo-cache.service redis-enketo-main.service
-sudo systemctl start redis-enketo-cache.service redis-enketo-main.service
-
-systemctl status redis-enketo-cache.service
-systemctl status redis-enketo-main.service
-
-echo "/var/log/redis/redis-enketo-cache.log:"
-sudo cat /var/log/redis/redis-enketo-cache.log
-
-echo "/var/log/redis/redis-enketo-main.log:"
-sudo cat /var/log/redis/redis-enketo-main.log
+sudo /usr/bin/redis-server $CONFIG_DIR/redis-enketo-cache.conf --supervised no --daemonize yes
+sudo /usr/bin/redis-server $CONFIG_DIR/redis-enketo-main.conf --supervised no --daemonize yes
 
 redis-cli -p 6379 ping
 
