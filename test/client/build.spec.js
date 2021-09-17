ccc98da0b39715c9455dd6b1af1802a8e1a45d0b
eyelidlessness <gnosis@gmail.com>
2021-09-17 15:07:39 -0700

    Restore Transformer/Core version test, correct spelling in preinstall script

--- test/client/build.spec.js
+++ test/client/build.spec.js
@@ -2,8 +2,7 @@ import pkg from '../../package.json';
 import { Form } from 'enketo-core';
 
 describe( 'Build checks: ', () => {
-    // TODO[2021-08-15]: This should be restored when Transformer and Core Node upgrades are released
-    it.skip( 'Transformer matches Core', () => {
+    it( 'Transformer matches Core', () => {
         expect( pkg.dependencies[ 'enketo-transformer' ] ).to.equal( Form.requiredTransformerVersion );
     } );
 } );
