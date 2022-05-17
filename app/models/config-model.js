4c5ed60495dc1c4ee3b5330a7cb96bb520c0579c
eyelidlessness <gnosis@gmail.com>
2022-05-17 15:54:11 -0700

    Prepare to release 4.0.0

--- app/models/config-model.js
+++ app/models/config-model.js
@@ -369,6 +369,7 @@ module.exports = {
         swipePage: config['swipe page'],
         textMaxChars: config['text field character limit'],
         csrfCookieName: config['csrf cookie name'],
+        excludeNonRelevant: config['exclude non-relevant'],
     },
     getThemesSupported,
 };
