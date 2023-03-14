949383ab780eec57d191c0ae463a2080d00f3f66
David Burke <david.burke@kobotoolbox.org>
2023-03-14 17:48:23 -0400

    Use pm2-runtime on docker/start.sh (#533)
    Fixes #531 by using pm2-runtime

--- setup/docker/start.sh
+++ setup/docker/start.sh
@@ -8,6 +8,6 @@ cd ${ENKETO_SRC_DIR}/
 # Create a config. file if necessary.
 python setup/docker/create_config.py
 
-# Run Enketo via PM2 (without daemonizing, so logs are exposed
-#   e.g. via `docker logs enketoexpress_enketo_1`).
-exec pm2 start --no-daemon app.js -n enketo
+# Run Enketo via PM2 Runtime (To support sigterm handling and
+# logs are exposed e.g. via `docker logs enketoexpress_enketo_1`).
+exec pm2-runtime app.js -n enketo
